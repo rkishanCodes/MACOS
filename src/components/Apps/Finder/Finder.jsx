ed194650cc885c4fa632d2c1f9b885a39346ad38
rkishanCodes <rkishan.codes@gmail.com>
2024-09-08 18:17:37 +0530

    working on ginie effect

--- src/components/Apps/Finder/Finder.jsx
+++ src/components/Apps/Finder/Finder.jsx
@@ -3,15 +3,15 @@ import ResizableWindow from "../ResizableWindow";
 import MenuActions from "../MenuActions";
 
 const Finder = React.memo(() => {
-  return (
-    <ResizableWindow appName="finder">
-      <div className="relative">
-        <MenuActions appName="finder" />
-        finder
-        <div onClick={() => console.log("clcikd finder")}>click me</div>
-      </div>
-    </ResizableWindow>
-  );
+return (
+<ResizableWindow appName="finder">
+<div className="relative">
+<MenuActions appName="finder" />
+finder
+<div onClick={() => console.log("clcikd finder")}>click me</div>
+</div>
+</ResizableWindow>
+);
 });
 
 export default Finder;
