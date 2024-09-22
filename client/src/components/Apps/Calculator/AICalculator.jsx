52ca2dc472ade20768c9f88740336c9b669b9ed6
rkishanCodes <rkishan.codes@gmail.com>
2024-09-22 20:54:30 +0530

    used unscroll in cal

--- client/src/components/Apps/Calculator/AICalculator.jsx
+++ client/src/components/Apps/Calculator/AICalculator.jsx
@@ -133,7 +133,6 @@ export default function Home({ containerSize }) {
       window.removeEventListener("DOMMouseScroll", preventDefault, false);
       window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
       window.removeEventListener("touchmove", preventDefault, wheelOpt);
-      window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
     }
 
     // Disable scroll on mount
